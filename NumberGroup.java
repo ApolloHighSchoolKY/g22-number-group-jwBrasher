d94cefaf51e81b0a7ab6ed74da2e9c9edea327d7
jwBrasher <john.brasher@stu.daviess.kyschools.us>
2025-04-09 13:29:56 +0000

    Idk.

---
+++ NumberGroup.java
@@ -0,0 +1,28 @@
+public class NumberGroup{
+
+    private int x;
+    private int y;
+    private int z;
+    
+
+    public int[] group{
+
+    }
+
+    public boolean contains(f){
+        
+
+
+        for(int i =0; i < group.length(); i++)
+        {
+            if(f == group[f])
+                return true;
+            else
+                return false;
+        }
+        
+
+    }
+
+
+}
